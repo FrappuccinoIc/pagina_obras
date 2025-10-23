c6609645ff12776e9cf2dfc8e8d07ebe1750ec6c
Frappuccinoic <arseeicore@gmail.com>
2025-10-23 13:47:10 -0300

    Optimización funciónes de paginar

--- catalogo/views.py
+++ catalogo/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Obra
 from django.core.paginator import Paginator
 
-def catalogo_lienzo(req):
-    obra = Obra.objects.filter(medio = "Lienzo").order_by("id")
+def catalogo_paginacion(req, medio):
+    obra = Obra.objects.filter(medio = medio).order_by("id")
     pag = Paginator(obra, 12)
 
     page_number = req.GET.get('page') # Se pasa la url que se quiere conseguir, en este caso: ?page=n
@@ -16,28 +16,28 @@ def catalogo_lienzo(req):
     end_index = min(index + 3, max_index)
     page_range = pag.page_range[start_index:end_index]
 
+    return [page_obj, page_range]
+
+def catalogo_lienzo(req):
+    medio = "Lienzo"
+    obj_return = catalogo_paginacion(req, medio)
+    page_obj = obj_return[0]
+    page_range = obj_return[1]
+
     return render(req, 'catalogo/catalogo.html', {
-        "medio": "Lienzo",
+        "medio": medio,
         "page_obj": page_obj,
         "page_range": page_range, 
     })
 
 def catalogo_impresiones(req):
-    obra = Obra.objects.filter(medio = "Impresiones").order_by("id")
-    pag = Paginator(obra, 12)
-
-    page_number = req.GET.get('page') # Se pasa la url que se quiere conseguir, en este caso: ?page=n
-    page_obj = pag.get_page(page_number)
-
-    # 🔹 Rango de páginas visibles (ejemplo: 5)
-    index = page_obj.number - 1  # Índice actual. Paginator.number nos da un número indexeado a 1, no a 0. Se resta para mantener una indexación de 0 para el rango
-    max_index = len(pag.page_range) # Consigue la cantidad de paginas en relación con la cantidad de objetos por página
-    start_index = max(index - 2, 0)
-    end_index = min(index + 3, max_index)
-    page_range = pag.page_range[start_index:end_index]
+    medio = "Impresiones"
+    obj_return = catalogo_paginacion(req, medio)
+    page_obj = obj_return[0]
+    page_range = obj_return[1]
 
     return render(req, 'catalogo/catalogo.html', {
-        "medio": "Impresiones",
+        "medio": medio,
         "page_obj": page_obj,
         "page_range": page_range, 
     })
