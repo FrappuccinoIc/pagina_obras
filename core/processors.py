9ce940144b186d7122b762aaf4e55a098b27bfcb
Frappuccinoic <arseeicore@gmail.com>
2025-10-23 12:32:23 -0300

    Página inicio hecha

--- core/processors.py
+++ core/processors.py
@@ -1,8 +1,6 @@
-
-
 def ctx_dict(request):
     ctx = {
-        "nombre_pagina": "Cristián Erre",
+        "nombre_pagina": "Cristián Erré",
         "LINK_INSTAGRAM": "https://www.instagram.com/cristian_erre/"
     }
     return ctx
