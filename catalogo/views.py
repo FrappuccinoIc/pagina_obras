07af9c5b0e05ed874e8f98048253d18cc50ee73d
Frappuccinoic <arseeicore@gmail.com>
2025-10-23 13:42:14 -0300

    Página de catálogo implementado

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
 from .models import Obra
+from django.core.paginator import Paginator
 
 def catalogo_lienzo(req):
-    obras = Obra.objects.filter(medio = "Lienzo")
-    return render(req, 'catalogo/catalogo.html', {"obras": obras, "medio": "Lienzo"})
+    obra = Obra.objects.filter(medio = "Lienzo").order_by("id")
+    pag = Paginator(obra, 12)
+
+    page_number = req.GET.get('page') # Se pasa la url que se quiere conseguir, en este caso: ?page=n
+    page_obj = pag.get_page(page_number)
+
+    # 🔹 Rango de páginas visibles (ejemplo: 5)
+    index = page_obj.number - 1  # Índice actual. Paginator.number nos da un número indexeado a 1, no a 0. Se resta para mantener una indexación de 0 para el rango
+    max_index = len(pag.page_range) # Consigue la cantidad de paginas en relación con la cantidad de objetos por página
+    start_index = max(index - 2, 0)
+    end_index = min(index + 3, max_index)
+    page_range = pag.page_range[start_index:end_index]
+
+    return render(req, 'catalogo/catalogo.html', {
+        "medio": "Lienzo",
+        "page_obj": page_obj,
+        "page_range": page_range, 
+    })
 
 def catalogo_impresiones(req):
-    obras = Obra.objects.filter(medio = "Impresiones")
-    return render(req, 'catalogo/catalogo.html', {"obras": obras, "medio": "Impresión"})
+    obra = Obra.objects.filter(medio = "Impresiones").order_by("id")
+    pag = Paginator(obra, 12)
+
+    page_number = req.GET.get('page') # Se pasa la url que se quiere conseguir, en este caso: ?page=n
+    page_obj = pag.get_page(page_number)
+
+    # 🔹 Rango de páginas visibles (ejemplo: 5)
+    index = page_obj.number - 1  # Índice actual. Paginator.number nos da un número indexeado a 1, no a 0. Se resta para mantener una indexación de 0 para el rango
+    max_index = len(pag.page_range) # Consigue la cantidad de paginas en relación con la cantidad de objetos por página
+    start_index = max(index - 2, 0)
+    end_index = min(index + 3, max_index)
+    page_range = pag.page_range[start_index:end_index]
+
+    return render(req, 'catalogo/catalogo.html', {
+        "medio": "Impresiones",
+        "page_obj": page_obj,
+        "page_range": page_range, 
+    })
 
 def detalles(req, obra_id):
     obra = Obra.objects.get(id = obra_id)
-    #obra = get_object_or_404(Obra,id = obra_id)
     return render(req, 'catalogo/detalles.html', {"obra": obra})
