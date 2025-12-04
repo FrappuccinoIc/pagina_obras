e464c1f8ac1e0a3b2b65b0a6595cb147748e76cc
Frappuccinoic <arseeicore@gmail.com>
2025-12-04 11:06:37 -0300

    Modelo usuarios creada

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+from catalogo.models import Obra
 
-# Create your models here.
+#class BoletaDeCompra:
---
+++ usuarios/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+class Usuario(models.Model):
+    username = models.CharField(max_length=40, verbose_name="Nombre de Usuario")
+    direccion = models.CharField(max_length=100)
+    cuenta = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Cuenta enlazada")
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+
+    def __str__(self): return self.username
