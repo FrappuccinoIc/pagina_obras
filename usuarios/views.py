eb4e64ddc7ae504fed644f9b6079294cc75f77b8
frappuccinoic <arseeicore@gmail.com>
2025-12-04 16:51:04 -0300

    Usuarios pueden acceder panel admin

--- usuarios/views.py
+++ usuarios/views.py
@@ -59,51 +59,13 @@ def registrar_usuario(req):
     grupo_perms = {
         "catalogo.view_obra, checkout.add_boletadecompra, catalogo.view_publicacion, catalogo.view_etiqueta"
     }
-    return registrar(req, False, "Usuarios", grupo_perms, 'usuarios/registrar.html')
+    return registrar(req, True, "Usuarios", grupo_perms, 'usuarios/registrar.html')
 
 def registrar_admin(req):
     grupo_perms = {
         "catalogo.view_publicacion, catalogo.delete_obra, catalogo.view_etiqueta, usuarios.view_usuario, contenttypes.view_contenttype, usuarios.add_usuario, catalogo.add_publicacion, contenttypes.delete_contenttype, checkout.delete_boletadecompra, usuarios.delete_usuario, catalogo.delete_etiqueta, auth.view_user, catalogo.change_publicacion, checkout.change_boletadecompra, auth.delete_user, catalogo.change_etiqueta, auth.view_permission, catalogo.delete_publicacion, catalogo.change_obra, contenttypes.change_contenttype, catalogo.add_etiqueta, auth.view_group, auth.add_user, usuarios.change_usuario, checkout.view_boletadecompra, auth.change_user, sessions.view_session, catalogo.view_obra, admin.view_logentry, contenttypes.add_contenttype, catalogo.add_obra"
     }
     return registrar(req, True, "Administradores", grupo_perms, 'usuarios/registrar_admin.html')
-
-""" def registrar_admin(req):
-    if req.method == 'POST':
-        form = UserForm(req.POST)
-
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            confirm_password = form.cleaned_data['confirm_password']
-
-            if password != confirm_password:
-                return redirect(reverse('registrar') + '?pass')
-
-            if User.objects.filter(username = username).exists():
-                return redirect(reverse('registrar') + '?fail')
-
-            nombre = form.cleaned_data['nombre']
-            apellidos = form.cleaned_data['apellidos']
-            direccion = form.cleaned_data['direccion']
-
-            user = User.objects.create_user(username = username, password = password)
-            usuario = Usuario.objects.create(username = username, cuenta = user, nombre = nombre, apellidos = apellidos, direccion = direccion)
-
-            user.is_staff = True
-            user.save()
-            usuario.save()
-            
-            group_perms = {}
-
-            group = conseguir_crear_grupo("Administradores", group_perms)
-            user.groups.add(group)
-
-            return redirect('home')
-
-    else:
-        form = UserForm()
-
-    return render(req, 'usuarios/registrar_admin.html', { 'form': form }) """
 
 @login_required
 def perfil(req):
