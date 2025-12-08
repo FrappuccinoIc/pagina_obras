d68310ae5ddb1af613e7435d761ea5cb028b488c
Frappuccinoic <arseeicore@gmail.com>
2025-12-08 16:32:17 -0300

    URL login actualizada

--- pagina_obras/urls.py
+++ pagina_obras/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('info/', views.info, name = "info"),
     path('pedidos/', views.pedidos, name = "pedidos"),
     path('admin/', admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
