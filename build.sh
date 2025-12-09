91d9989b761611f0b8b0ddcea1251e3d9748e65a
Alfonso Espinoza <alfonso.espinoza19@gmail.com>
2025-12-09 06:18:39 -0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
 
 python manage.py migrate
 
-python manage.py loaddata data.json
+# python manage.py loaddata data.json
