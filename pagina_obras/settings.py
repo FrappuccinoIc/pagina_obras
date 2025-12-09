63371bbff483718c8e9a85028f0604550c234e83
Alfonso Espinoza <alfonso.espinoza19@gmail.com>
2025-12-09 04:00:27 -0300

    Eliminado STATIC_ROOT en modo debug

--- pagina_obras/settings.py
+++ pagina_obras/settings.py
@@ -127,8 +127,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR
-
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
