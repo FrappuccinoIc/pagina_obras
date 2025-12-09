af0d05e7f88b4f4d2c057798f5620879e97b79ad
Alfonso Espinoza <alfonso.espinoza19@gmail.com>
2025-12-09 07:43:24 -0300

    actualizadas imagenes obras

--- pagina_obras/settings.py
+++ pagina_obras/settings.py
@@ -137,8 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+if DEBUG:
+    MEDIA_URL='/media/'
+    MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+else:
+    MEDIA_URL='https://res.cloudinary.com/dwk9xy3hs/image/upload/'
+    MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+    
 CART_SESSION_ID = 'carrito'
 
 
