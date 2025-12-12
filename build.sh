1e2654c067b78444973d96516b3a669436806ec5
Alfonso <alfonso.espinoza19@gmail.com>
2025-12-11 22:05:18 -0300

    4to cambio build script - no migrate

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ python manage.py collectstatic --no-input
 
 # python manage.py flush --no-input
 
-python manage.py makemigrations
+# python manage.py makemigrations
 
-python manage.py migrate
+# python manage.py migrate
 
-python manage.py loaddata data.json
+# python manage.py loaddata data.json
